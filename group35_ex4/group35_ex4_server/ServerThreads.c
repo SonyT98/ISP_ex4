cb199d39345603424e449a20c8e1a97c9383769b
SonyT98 <57562876+SonyT98@users.noreply.github.com>
2020-01-10 17:03:31 +0200

    ending endgamestatus and create service thread for testing

--- group35_ex4/group35_ex4_server/ServerThreads.c
+++ group35_ex4/group35_ex4_server/ServerThreads.c
@@ -1,7 +1,7 @@
 
 #include "ServerThreads.h"
 #include "SharedHardCodedData.h"
-
+#include "ServiceFunctions.h"
 
 
 DWORD WINAPI AcceptClientThread(LPSTR lpParam)
@@ -33,4 +33,25 @@ DWORD WINAPI CheckExitThread(LPSTR lpParam)
 		}
 	}
 	return ERROR_CODE;
+}
+
+DWORD WINAPI ServiceThread(LPSTR lpParam)
+{
+	ServiceThreadParams *arg = NULL;
+
+	arg = (ServiceThreadParams*)lpParam;
+
+	char username[MAX_USERNAME];
+	char player_move[MAX_MESSAGE];
+	char cpu_move[MAX_MESSAGE];
+	int err = 0;
+	int menu_selection = 0, winning_player = 0, replay = 0;
+
+	err = ClientUsername(arg->client_socket, username);
+
+	err = SelectFromMenu(arg->client_socket, &menu_selection);
+
+	err = CPUGame(arg->client_socket, player_move, cpu_move, &winning_player);
+
+	err = EndGameStatus(arg->client_socket, username, "CPU", player_move, cpu_move, winning_player, &replay);
 }

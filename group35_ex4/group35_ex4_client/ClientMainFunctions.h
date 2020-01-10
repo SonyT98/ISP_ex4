22cbb9c8cade0bccaf2c7ef51014924cbac762ed
nadavnave <57562882+nadavnave@users.noreply.github.com>
2020-01-10 15:44:57 +0200

    client basic functionality for debugging

--- group35_ex4/group35_ex4_client/ClientMainFunctions.h
+++ group35_ex4/group35_ex4_client/ClientMainFunctions.h
@@ -3,6 +3,7 @@
 #define __CLENTMAINFUNCTIONS_H__
 
 #include "ThreadFunctions.h"
+#include "SharedHardCodedData.h"
 
 
 /*
@@ -12,6 +13,14 @@
 */
 int clientMain();
 
+/*
+* This function initilize the client socket and bind it to a port. than try to connect to the server.
+* Arguments:
+*	sock - pointer to asocket to be updated as a connected socket to the server
+* Return:
+*	the funciton return ERROR_CODE if coudent connect or error accrued 
 
+*/
+int initializeConnection(SOCKET *sock);
 
 #endif // __CLIENTMAINFUNCTIONS_H__
